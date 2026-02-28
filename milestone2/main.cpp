cac2761ff0d98084912c7f39024bc6ba4886a961
JokerTux <patrick.slomian90@gmail.com>
2026-02-28 12:31:01 +0100

    painful vulkan - not ready yet

--- milestone2/main.cpp
+++ milestone2/main.cpp
@@ -123,13 +123,48 @@ class Vulkan_init{
 		VkInstance get_instance() const {return instance;}
 		VkSurfaceKHR get_surface() const {return surface;}
 
+		bool create_instance(const std::vector<const char*> &extensions){
+			//metadata
+		    VkApplicationInfo app_info{VK_STRUCTURE_TYPE_APPLICATION_INFO};
+		    app_info.pApplicationName = "First vulkan";//metadata
+		    app_info.applicationVersion = VK_MAKE_VERSION(0, 1, 0);//metadata
+		    app_info.pEngineName = "Test engine";//metadata
+		    app_info.engineVersion = VK_MAKE_VERSION(0, 1, 0);//metadata
+		    app_info.apiVersion = VK_API_VERSION_1_2;//usable api version
+
+		    //debug stuff | prints debug messages into the console
+		    #ifndef NDEBUG
+			const char *layers[1] = {"VK_LAYER_KHRONOS_validation"};
+			const uint32_t layer_count = 1;
+			#else
+			const char **layers = nullptr;
+			const uint32_t layer_count = 0;
+			#endif
+		    
+		    VkInstanceCreateInfo i_data{VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
+		    i_data.pApplicationInfo = &app_info;
+		    i_data.enabledExtensionCount = (uint32_t)extensions.size(); 
+		    i_data.ppEnabledExtensionNames = extensions.data();
+		    i_data.enabledLayerCount = layer_count;
+		    i_data.ppEnabledLayerNames = (layer_count > 0) ? layers : nullptr;
+
+		    VkResult r = vkCreateInstance(&i_data, nullptr, &instance);
+
+		   	if(r != VK_SUCCESS){
+		    	std::cerr << "vkCreateInstance failed : " << r << '\n';
+		    	instance = VK_NULL_HANDLE;
+		    	return false;
+		    }
+
+		    return true;
+		}
 
 		bool surface_vk(SDL_Window* window){
 			if(!SDL_Vulkan_CreateSurface(window, instance, &surface)){
 				std::cerr << "SDL_Vulkan_CreateSurface error : " << SDL_GetError() << '\n';
 				if(surface != VK_NULL_HANDLE){
-					surface = VK_NULL_HANDLE;
 					vkDestroySurfaceKHR(instance, surface, nullptr);
+					surface = VK_NULL_HANDLE;
 				}
 
 				return false;
@@ -295,7 +330,186 @@ class Vulkan_init{
 			return true;
 		}
 
+		bool queues_vk(){
+			//queueIndex = 0 bcs we only have one element in gpu and present families.
+			vkGetDeviceQueue(device, export_index_gpu_graphic_q, 0, &gpu_queue);
+			vkGetDeviceQueue(device, export_index_gpu_present_q, 0, &present_queue);
+			
+			if(gpu_queue == VK_NULL_HANDLE || present_queue == VK_NULL_HANDLE){
+				return false;
+			}
+
+			std::cout << "queues found" << std::endl;
+
+			return true;
+		}
+
+		bool sur_cap_KHR(){
+			VkResult sur_KHR = vkGetPhysicalDeviceSurfaceCapabilitiesKHR(phys_dev, surface, &pSurfaceCapabilities);
+			std::cout << "minImageCount :" << pSurfaceCapabilities.minImageCount << std::endl;
+			std::cout << "Max Img Count :" << pSurfaceCapabilities.maxImageCount << std::endl;
+			std::cout << "currentExtent.width :" << pSurfaceCapabilities.currentExtent.width << std::endl;
+			std::cout << "currentExtent.height :" << pSurfaceCapabilities.currentExtent.height << std::endl;
+			std::cout << "currentTransform :" << pSurfaceCapabilities.currentTransform << std::endl;
+
+			VkResult phys_sur_KHR = vkGetPhysicalDeviceSurfaceFormatsKHR(phys_dev, surface, &pSurfaceFormatCount, nullptr);
+			pSurfaceFormats.resize(pSurfaceFormatCount);
+			if(pSurfaceFormats.empty()) return false;
+			phys_sur_KHR = vkGetPhysicalDeviceSurfaceFormatsKHR(phys_dev, surface, &pSurfaceFormatCount, pSurfaceFormats.data());
+
+			VkResult phys_sur_pre_mode_KHR = vkGetPhysicalDeviceSurfacePresentModesKHR(phys_dev, surface, &pPresentModeCount, nullptr);
+			pPresentModes.resize(pPresentModeCount);
+			phys_sur_pre_mode_KHR = vkGetPhysicalDeviceSurfacePresentModesKHR(phys_dev, surface, &pPresentModeCount, pPresentModes.data());
+
+			if(sur_KHR == VK_SUCCESS && phys_sur_KHR == VK_SUCCESS && phys_sur_pre_mode_KHR == VK_SUCCESS){
+				return true;
+			}
+	
+			return false;
+		}
+
+		bool des_formats_chk(){
+			bool found = false;
+
+			if(pSurfaceFormats.empty()) return false;
+
+			for(uint32_t a2 = 0; a2 < pSurfaceFormatCount; a2++){
+				//selecting the required format
+				if(pSurfaceFormats[a2].format == VK_FORMAT_B8G8R8A8_SRGB && pSurfaceFormats[a2].colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR){
+					chosenFormat = pSurfaceFormats[a2];
+					found = true;
+					break;
+				}
+			}
+
+			if(!found){
+				chosenFormat = pSurfaceFormats[0];
+				found = true;
+			}
+			return found;
+		}	
+
+		bool modes_chk(){
+			//mailbox cares abut the newest frame|fifo(first in first out) shows every frame even when late.
+			if(!pPresentModeCount || pPresentModes.empty() || pPresentModeCount	!= pPresentModes.size()){
+				return false;
+			}
+
+			chosenPresentMode = VK_PRESENT_MODE_FIFO_KHR;
+
+			for(uint32_t a1 = 0; a1 < pPresentModeCount; a1++){
+				/*std::cout << "Value : " << std::endl;
+				std::cout << pPresentModes.data()[a1] << std::endl;*/
+				
+				if(pPresentModes[a1] == VK_PRESENT_MODE_MAILBOX_KHR){
+					chosenPresentMode = VK_PRESENT_MODE_MAILBOX_KHR;
+					return true;
+				}
+
+			}
+			return true;
+		}
+
+		bool swap_init(){
+			uint32_t indices[] = {export_index_gpu_graphic_q, export_index_gpu_present_q};
+
+			VkSwapchainCreateInfoKHR swapInfo{VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR};
+			swapInfo.surface = surface;
+			swapInfo.minImageCount = pSurfaceCapabilities.minImageCount + 1;
+			swapInfo.imageFormat = chosenFormat.format;
+			swapInfo.imageColorSpace = chosenFormat.colorSpace;
+			swapInfo.imageExtent = pSurfaceCapabilities.currentExtent;
+			swapInfo.imageArrayLayers = 1; //2d - no depth 
+			swapInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+			swapInfo.preTransform = pSurfaceCapabilities.currentTransform;
+			swapInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+			swapInfo.presentMode = chosenPresentMode;
+			swapInfo.clipped = VK_TRUE;
+
+			swapInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+			if(export_index_gpu_graphic_q != export_index_gpu_present_q){
+				swapInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+				swapInfo.queueFamilyIndexCount = 2;
+				swapInfo.pQueueFamilyIndices = indices;
+			}
+
+			if(vkCreateSwapchainKHR(device, &swapInfo, nullptr, &swapchain) == VK_SUCCESS){
+				return true;	
+			}
+			else{
+				return false;
+			}
+			
+		}
+
+		bool re_swap_init(){
+			uint32_t indices[] = {export_index_gpu_graphic_q, export_index_gpu_present_q};
+
+			//wait until gpu is not using the swapchain anymore
+			vkDeviceWaitIdle(device);
+
+			VkSwapchainKHR old = swapchain;
+
+			VkSwapchainCreateInfoKHR re_swapInfo{VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR};
+			re_swapInfo.surface = surface;
+			re_swapInfo.minImageCount = pSurfaceCapabilities.minImageCount + 1;
+			re_swapInfo.imageFormat = chosenFormat.format;
+			re_swapInfo.imageColorSpace = chosenFormat.colorSpace;
+			re_swapInfo.imageExtent = pSurfaceCapabilities.currentExtent;
+			re_swapInfo.imageArrayLayers = 1; //2d - no depth 
+			re_swapInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+			re_swapInfo.preTransform = pSurfaceCapabilities.currentTransform;
+			re_swapInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+			re_swapInfo.presentMode = chosenPresentMode;
+			re_swapInfo.clipped = VK_TRUE;
+
+			re_swapInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+			if(export_index_gpu_graphic_q != export_index_gpu_present_q){
+				re_swapInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+				re_swapInfo.queueFamilyIndexCount = 2;
+				re_swapInfo.pQueueFamilyIndices = indices;
+			}
+
+			re_swapInfo.oldSwapchain = old;
+
+			VkSwapchainKHR n_swapchain = VK_NULL_HANDLE;
+
+			VkResult r_re_swap = vkCreateSwapchainKHR(device, &re_swapInfo, nullptr, &n_swapchain);
+			if(r_re_swap != VK_SUCCESS) return false;
+
+			if(old != VK_NULL_HANDLE){
+				vkDestroySwapchainKHR(device, old, nullptr);
+			}
+
+			swapchain = n_swapchain;
+			return true;			
+		}
+
+		bool swap_get_images(){
+			VkResult get_img_swap = vkGetSwapchainImagesKHR(device, swapchain, &pSwapchainImageCount, nullptr);
+			
+			std::cout << pSwapchainImageCount << std::endl;
+			
+			if(get_img_swap == VK_SUCCESS){
+				swapchainImages.resize(pSwapchainImageCount);
+				VkResult swap_imgs = vkGetSwapchainImagesKHR(device, swapchain, &pSwapchainImageCount, swapchainImages.data());
+				if(swap_imgs == VK_SUCCESS) return true;
+			}
+			/*for(uint32_t a111 = 0; a111 < pSwapchainImageCount; a111++){
+				std::cout << swapchainImages.data()[a111] << std::endl;
+			}*/				
+			return false;	
+		}
+
 		~Vulkan_init(){
+			if(swapchain != VK_NULL_HANDLE){
+				vkDestroySwapchainKHR(device, swapchain, nullptr);
+				swapchain = VK_NULL_HANDLE;
+				std::cerr << "vkDestroySwapchainKHR \n";
+			}
+
 			if(device != VK_NULL_HANDLE){
 				vkDestroyDevice(device, nullptr);
 				device = VK_NULL_HANDLE;
@@ -320,48 +534,23 @@ class Vulkan_init{
 		VkSurfaceKHR surface = VK_NULL_HANDLE;
 		VkPhysicalDevice phys_dev = VK_NULL_HANDLE;
 		VkDevice device = VK_NULL_HANDLE;
+		VkQueue gpu_queue = VK_NULL_HANDLE;
+		VkQueue present_queue = VK_NULL_HANDLE;
+		VkSwapchainKHR swapchain = VK_NULL_HANDLE;
+		std::vector<VkSurfaceFormatKHR> pSurfaceFormats;
+		VkSurfaceFormatKHR chosenFormat = {};
+		std::vector<VkPresentModeKHR> pPresentModes;
+		std::vector<VkImage> swapchainImages;
+		VkPresentModeKHR chosenPresentMode = {};
+		VkSurfaceCapabilitiesKHR pSurfaceCapabilities = {};
 		VkPhysicalDeviceFeatures support_gpu{};
 		uint32_t pQueueFamilyPropertyCount = 0;
 		uint32_t export_index_gpu_graphic_q = 0; // very important for later.
 		uint32_t export_index_gpu_present_q = 0; // very important for later.
 		uint32_t pPropertyCount = 0;
-
-		bool create_instance(const std::vector<const char*> &extensions){
-			//metadata
-		    VkApplicationInfo app_info{VK_STRUCTURE_TYPE_APPLICATION_INFO};
-		    app_info.pApplicationName = "First vulkan";//metadata
-		    app_info.applicationVersion = VK_MAKE_VERSION(0, 1, 0);//metadata
-		    app_info.pEngineName = "Test engine";//metadata
-		    app_info.engineVersion = VK_MAKE_VERSION(0, 1, 0);//metadata
-		    app_info.apiVersion = VK_API_VERSION_1_2;//usable api version
-
-		    //debug stuff | prints debug messages into the console
-		    #ifndef NDEBUG
-			const char *layers[1] = {"VK_LAYER_KHRONOS_validation"};
-			const uint32_t layer_count = 1;
-			#else
-			const char **layers = nullptr;
-			const uint32_t layer_count = 0;
-			#endif
-		    
-		    VkInstanceCreateInfo i_data{VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
-		    i_data.pApplicationInfo = &app_info;
-		    i_data.enabledExtensionCount = (uint32_t)extensions.size(); 
-		    i_data.ppEnabledExtensionNames = extensions.data();
-		    i_data.enabledLayerCount = layer_count;
-		    i_data.ppEnabledLayerNames = (layer_count > 0) ? layers : nullptr;
-
-		    VkResult r = vkCreateInstance(&i_data, nullptr, &instance);
-
-		   	if(r != VK_SUCCESS){
-		    	std::cerr << "vkCreateInstance failed : " << r << '\n';
-		    	instance = VK_NULL_HANDLE;
-		    	return false;
-		    }
-
-		    return true;
-		}
-
+		uint32_t pSurfaceFormatCount = 0;
+		uint32_t pPresentModeCount = 0;
+		uint32_t pSwapchainImageCount = 0;
 };
 
 
@@ -378,10 +567,18 @@ int main(int argc, char** argv){
 		if(extensions.empty()) return 1;
 
 		Vulkan_init vulkan_init;
+		
 		if(!vulkan_init.init(extensions)) return 1;
 		if(!vulkan_init.surface_vk(display_window.get_window())) return	1;
 		if(!vulkan_init.select_device()) return	1;
 		if(!vulkan_init.create_device()) return 1;
+		if(!vulkan_init.queues_vk()) return 1;
+		if(!vulkan_init.sur_cap_KHR()) return 1;
+		if(!vulkan_init.des_formats_chk()) return 1;
+		if(!vulkan_init.modes_chk()) return 1;
+		if(!vulkan_init.swap_init()) return 1;
+		if(!vulkan_init.swap_get_images()) return 1;
+		if(!vulkan_init.re_swap_init()) return 1;
 		
 		while(display_window.run_window){
 				display_window.input_from_usr();
